3782fc07de3e61479ac3edcfdfe9271fa8c3faf2
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-01-16 22:40:19 -0600

    add a main function

--- hangman.py
+++ hangman.py
@@ -1,8 +1,5 @@
 # Hangman
 
-lives = 10
-word = "Apple"
-
 def prompt_user(word, lives):
     sentence = "Welcome! The word has "
     word_len = len(word)
@@ -25,11 +22,25 @@ def verify_answer(guess, word):
     for character in word:
         if guess == character:
             result += guess + " "
+            life = 0 #indicate that the answer is right and no life will be taken
         else:
             result += "_ "
+            life = -1 #wrong answer, take 1 life
     print(result)
+    return (life, result)
 
+def hangman():
+    lives = 10
+    word = "Apple"
 
-prompt_user(word, lives)
-guess = input("Please take a guess.\n")
-verify_answer(guess.lower(), word)
+    while lives > 0:
+        prompt_user(word, lives)
+        guess = input("Please take a guess.\n")
+        (life, result) = verify_answer(guess.lower(), word)
+        if "_" in result:
+             # still haven't got the word, continue
+            lives += life
+        else:
+            print("You won! The word is: " + word)
+
+hangman()
